a5e74bb36eef91a20bb7138bd6efa386433f7679
fbsc1999 <49154160+fbsc1999@users.noreply.github.com>
2021-03-06 16:23:15 +0000

    Caixa Generator

--- Generator/Generator/Main.cpp
+++ Generator/Generator/Main.cpp
@@ -10,7 +10,7 @@
 * Esses pontos, depois de calculados, devem ser escritos no ficheiro
 */
 void desenhaPlano(FILE* f, float tamLado) {
-	fprintf(f, "6\n");   //escrita inicial do número de pontos
+	fprintf(f, "6\n");   //escrita inicial do número de pontos/vértices
 
 	float h = 0.0f;     //todos os pontos terão y == 0
 
@@ -24,7 +24,72 @@ void desenhaPlano(FILE* f, float tamLado) {
 	fprintf(f, "%f %f %f\n", -tamLado / 2, h, tamLado / 2);
 }
 
+/**
+* Função responsável por calcular os pontos que descrevem os 12 triângulos
+* que permitem desenhar a caixa centrada na origem
+* Esses pontos, depois de calculados, devem ser escritos no ficheiro
+*/
 void desenhaCaixa(FILE* f, float tamX, float tamY, float tamZ) {
+	fprintf(f, "36\n");   //escrita inicial do número de pontos/vértices
+
+	float hMax = tamY / 2;
+	float x = tamX / 2;
+	float z = tamZ / 2;
+
+	//calculo e escrita dos pontos dos triângulos da tampa (ordem mão direita, y fixo):
+	fprintf(f, "%f %f %f\n", x, hMax, z);
+	fprintf(f, "%f %f %f\n", x, hMax, -z);
+	fprintf(f, "%f %f %f\n", -x, hMax, z);
+
+	fprintf(f, "%f %f %f\n", x, hMax, -z);
+	fprintf(f, "%f %f %f\n", -x, hMax, -z);
+	fprintf(f, "%f %f %f\n", -x, hMax, z);
+
+	//calculo e escrita dos pontos dos triângulos da base (ordem mão esquerda):
+	fprintf(f, "%f %f %f\n", x, -hMax, z);
+	fprintf(f, "%f %f %f\n", -x, -hMax, z);
+	fprintf(f, "%f %f %f\n", x, -hMax, -z);
+
+	fprintf(f, "%f %f %f\n", x, -hMax, -z);
+	fprintf(f, "%f %f %f\n", -x, -hMax, z);
+	fprintf(f, "%f %f %f\n", -x, -hMax, -z);
+
+	//calculo e escrita dos pontos dos triângulos das faces laterais:
+	//1ª fase ("frente", z fixo == tamZ/2):
+	fprintf(f, "%f %f %f\n", x, -hMax, z);
+	fprintf(f, "%f %f %f\n", x, hMax, z);
+	fprintf(f, "%f %f %f\n", -x, -hMax, z);
+
+	fprintf(f, "%f %f %f\n", x, hMax, z);
+	fprintf(f, "%f %f %f\n", -x, hMax, z);
+	fprintf(f, "%f %f %f\n", -x, -hMax, z);
+
+	//2ª fase ("traseira", z fixo == -tamZ/2):
+	fprintf(f, "%f %f %f\n", x, hMax, -z);
+	fprintf(f, "%f %f %f\n", x, -hMax, -z);
+	fprintf(f, "%f %f %f\n", -x, -hMax, -z);
+
+	fprintf(f, "%f %f %f\n", x, hMax, -z);
+	fprintf(f, "%f %f %f\n", -x, -hMax, -z);
+	fprintf(f, "%f %f %f\n", -x, hMax, -z);
+
+	//3ª fase ("direita", x fixo == tamZ/2):
+	fprintf(f, "%f %f %f\n", x, -hMax, -z);
+	fprintf(f, "%f %f %f\n", x, hMax, -z);
+	fprintf(f, "%f %f %f\n", x, -hMax, z);
+
+	fprintf(f, "%f %f %f\n", x, hMax, -z);
+	fprintf(f, "%f %f %f\n", x, hMax, z);
+	fprintf(f, "%f %f %f\n", x, -hMax, z);
+
+	//4ª fase ("esquerda, x fixo == -tamZ/2):
+	fprintf(f, "%f %f %f\n", -x, hMax, -z);
+	fprintf(f, "%f %f %f\n", -x, -hMax, -z);
+	fprintf(f, "%f %f %f\n", -x, -hMax, z);
+
+	fprintf(f, "%f %f %f\n", -x, hMax, -z);
+	fprintf(f, "%f %f %f\n", -x, -hMax, z);
+	fprintf(f, "%f %f %f\n", -x, hMax, z);
 
 }
 
