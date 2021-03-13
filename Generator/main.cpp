8fd1d9110b91e58f51a888f0d462ee93f8dc9c76
Nellkam <manuelworkplace@gmail.com>
2021-03-13 16:41:42 +0000

    main atualizado

--- Generator/main.cpp
+++ Generator/main.cpp
@@ -3,27 +3,13 @@
 #include <string>
 #include "./headers/Generator.h"
 
-#define XML "configuration.xml"
-
-using namespace std;
-
 int main(int argc,char** argv)
 {
     Generator generator(argc,argv);
 
-    // pontos modelo
-    string model = generator.modelo();
+    generator.writeModelo();
 
-    ofstream modelFile(*generator.fileName);
-    ofstream xmlFile(XML);
-
-    // escrita ficheiro .3d
-    modelFile << model;
-    modelFile.close();
-    model.clear();
-    cout << "Ficheiro gerado: " << *generator.fileName << endl;
-
-    //escrita XML
+    std::cout << "Ficheiro gerado: " << *generator.fileName << std::endl;
 
     return 1;
 }
