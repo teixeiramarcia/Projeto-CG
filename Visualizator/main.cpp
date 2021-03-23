34c60149810c6201d34769de86042c28784cf741
teixeiramarcia <filipa.teixeira98@gmail.com>
2021-03-23 17:08:04 +0000

    Add camera motion with keys

--- Visualizator/main.cpp
+++ Visualizator/main.cpp
@@ -26,6 +26,15 @@ GLfloat z = 0.0f;
 float alpha = 0.0f;
 int drawingType = GL_LINE;
 
+float alfa = 0.0f, beta = 0.0f, radius = 5.0f;
+float camX, camY, camZ;
+
+void spherical2Cartesian() {
+    camX = radius * cos(beta) * sin(alfa);
+    camY = radius * sin(beta);
+    camZ = radius * cos(beta) * cos(alfa);
+}
+
 void changeSize(int w, int h) {
 
 	// Prevent a divide by zero, when window is too short
@@ -56,7 +65,7 @@ void renderScene() {
 
 	// set the camera
 	glLoadIdentity();
-    gluLookAt(5.0,5.0,5.0,
+    gluLookAt(camX,camY,camZ,
               0.0,0.0,0.0,
               0.0f,1.0f,0.0f);
 
@@ -182,6 +191,45 @@ void keyboardCallback(unsigned char key_code, int _unused1, int _unused2) {
     glutPostRedisplay();
 }
 
+// write function to process keyboard events
+void processSpecialKeys(int key, int xx, int yy) {
+    switch (key) {
+        case GLUT_KEY_RIGHT:
+            alfa -= 0.1;
+            break;
+
+        case GLUT_KEY_LEFT:
+            alfa += 0.1;
+            break;
+
+        case GLUT_KEY_UP:
+            beta += 0.1f;
+            if (beta > 1.5f)
+                beta = 1.5f;
+            break;
+
+        case GLUT_KEY_DOWN:
+            beta -= 0.1f;
+            if (beta < -1.5f)
+                beta = -1.5f;
+            break;
+
+        case GLUT_KEY_PAGE_DOWN:
+            radius -= 0.1f;
+            if (radius < 0.1f)
+                radius = 0.1f;
+            break;
+
+        case GLUT_KEY_PAGE_UP:
+            radius += 0.1f;
+            break;
+        default:
+            break;
+    }
+    spherical2Cartesian();
+    glutPostRedisplay();
+}
+
 
 vector<string> splitter(string& str, char delimiter) {
     stringstream ss(str);
@@ -239,12 +287,16 @@ void glutSetup(int argc, char **argv) {
     glutDisplayFunc(renderScene);
     glutReshapeFunc(changeSize);
     glutIdleFunc(renderScene);
+
+    // put here the registration of the keyboard callbacks
     glutKeyboardFunc(keyboardCallback);
+    glutSpecialFunc(processSpecialKeys);
 
     // OpenGL settings
 	glEnable(GL_DEPTH_TEST);
 	//glEnable(GL_CULL_FACE);
 	glClearColor(0.0f,0.0f,0.0f,0.0f);
+    spherical2Cartesian();
 }
 
 int main(int argc, char **argv) {
