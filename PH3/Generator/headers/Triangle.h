c5048c86406264868618d1064d2def7fe1a28599
Nellkam <manuelworkplace@gmail.com>
2021-05-01 15:14:05 +0100

    adicionado construtor copia para triangulo -> fix double free

--- PH3/Generator/headers/Triangle.h
+++ PH3/Generator/headers/Triangle.h
@@ -11,11 +11,14 @@ typedef class Triangle
     public:
         Triangle(double x1,double y1,double z1,double x2,double y2,double z2,double x3,double y3,double z3);
         Triangle(PPoint3D p1,PPoint3D p2,PPoint3D p3);
+        Triangle(const Point3D &p1,const Point3D &p2, const Point3D &p3);
         Triangle(Triangle* t);
+        Triangle(const Triangle &t);
         ~Triangle();
 
         PPoint3D getPoint(int i);
+        std::vector<PPoint3D> getPoints() const;
         void setPoint(int i,PPoint3D p);
         std::string toString();
-    
+
 }* PTriangle;
